b962c78f20529b29ae8e82fb6d9bcf1b110b3d43
simonachess <simonachess@gmail.com>
2025-01-07 18:52:30 +0200

    testing

--- src/components/OpeningQuiz.vue
+++ src/components/OpeningQuiz.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Chess Opening Quiz</h1>
+    <h1>Chess Opening Quiz!</h1>
 
     <!-- Quiz Questions Section -->
     <div v-if="currentQuestionIndex < questions.length">
@@ -11,7 +11,10 @@
         </div>
       </div>
       <div v-else>
-        <div v-for="(option, index) in questions[currentQuestionIndex].options" :key="index">
+        <div
+          v-for="(option, index) in questions[currentQuestionIndex].options"
+          :key="index"
+        >
           <button @click="selectOption(option.value)">{{ option.text }}</button>
         </div>
       </div>
@@ -34,7 +37,7 @@
     <div v-if="currentQuestionIndex >= questions.length">
       <ChessBoardComponent
         :moves="convertMovesToArray(openingDetails ? openingDetails.Moves : '')"
-        />
+      />
     </div>
 
     <!-- Debugging Section to Display Selected Answers and Result -->
@@ -44,30 +47,28 @@
       <p><strong>Computed Key:</strong> {{ selectedAnswers.join(" - ") }}</p>
       <p><strong>ECO Code:</strong> {{ resultEcoCode }}</p>
       <p><strong>Opening Details:</strong> {{ openingDetails }}</p>
-
     </div>
   </div>
 </template>
 
 <script>
-import questions from '@/data/questions.json';
-import results from '@/data/results.json';
-import ecoCodes from '@/data/eco-codes.json';
-import ChessBoardComponent from '@/components/ChessBoardComponent.vue';
+import questions from "@/data/questions.json";
+import results from "@/data/results.json";
+import ecoCodes from "@/data/eco-codes.json";
+import ChessBoardComponent from "@/components/ChessBoardComponent.vue";
 
 export default {
   components: {
-    ChessBoardComponent
+    ChessBoardComponent,
   },
   data() {
-
     return {
       currentQuestionIndex: 0,
       selectedAnswers: [],
       questions: questions,
       ecoCodes: ecoCodes,
-      initialFen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-      initialMoves: []
+      initialFen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+      initialMoves: [],
     };
   },
   computed: {
@@ -79,7 +80,7 @@ export default {
     openingDetails() {
       const ecoCode = this.resultEcoCode;
       return this.ecoCodes[ecoCode] || null;
-    }
+    },
   },
   methods: {
     selectOption(value) {
@@ -107,30 +108,30 @@ export default {
       }
     },
     handleSecondQuestion(value) {
-      if (value === 'white') {
+      if (value === "white") {
         this.currentQuestionIndex = 2;
-      } else if (value === 'black') {
+      } else if (value === "black") {
         this.currentQuestionIndex = 3;
-      } else if (value === '') {
+      } else if (value === "") {
         this.currentQuestionIndex = 6;
         this.selectedAnswers.push("", "");
       }
     },
     handleThirdQuestion(value) {
-      if (value === 'e4') {
+      if (value === "e4") {
         this.currentQuestionIndex = 4;
-      } else if (value === 'd4') {
+      } else if (value === "d4") {
         this.currentQuestionIndex = 5;
-      } else if (value === 'other' || value === '') {
+      } else if (value === "other" || value === "") {
         this.currentQuestionIndex = 6;
         this.selectedAnswers.push("");
       }
     },
     handleBlackThirdQuestion(value) {
-      if (value === 'e4' || value === 'd4') {
+      if (value === "e4" || value === "d4") {
         this.selectedAnswers.push("");
         this.currentQuestionIndex = 6;
-      } else if (value === 'other' || value === '') {
+      } else if (value === "other" || value === "") {
         this.selectedAnswers.push("");
         this.currentQuestionIndex = 6;
       } else {
@@ -139,53 +140,62 @@ export default {
     },
     shouldRemoveTrading() {
       const combinations = [
-        ['advanced', 'black', 'other', ''],
-        ['intermediate', 'black', 'other', ''],
-        ['advanced', 'white', 'e4', 'c5'],
-        ['intermediate', 'white', 'e4', 'c5'],
-        ['advanced', 'white', 'e4', 'other'],
-        ['intermediate', 'white', 'e4', 'other'],
-        ['advanced', 'white', 'other', ''],
-        ['intermediate', 'white', 'other', ''],
-        ['beginner', 'white', 'd4', 'Nf6'],
-        ['beginner', 'black', 'other', ''],
-        ['beginner', 'white', 'e4', 'c5'],
-        ['beginner', 'white', 'e4', 'other'],
-        ['beginner', 'white', 'other', ''],
-        ['advanced', 'white', 'd4', 'other'],
-        ['intermediate', 'white', 'd4', 'other'],
-        ['beginner', 'white', 'd4', 'other'],
+        ["advanced", "black", "other", ""],
+        ["intermediate", "black", "other", ""],
+        ["advanced", "white", "e4", "c5"],
+        ["intermediate", "white", "e4", "c5"],
+        ["advanced", "white", "e4", "other"],
+        ["intermediate", "white", "e4", "other"],
+        ["advanced", "white", "other", ""],
+        ["intermediate", "white", "other", ""],
+        ["beginner", "white", "d4", "Nf6"],
+        ["beginner", "black", "other", ""],
+        ["beginner", "white", "e4", "c5"],
+        ["beginner", "white", "e4", "other"],
+        ["beginner", "white", "other", ""],
+        ["advanced", "white", "d4", "other"],
+        ["intermediate", "white", "d4", "other"],
+        ["beginner", "white", "d4", "other"],
       ];
 
-      return combinations.some(combination => {
-        return combination.every((answer, index) => this.selectedAnswers[index] === answer);
+      return combinations.some((combination) => {
+        return combination.every(
+          (answer, index) => this.selectedAnswers[index] === answer
+        );
       });
     },
     getPlayStyleOptions() {
       let options = [
         { text: "Attacking. I love to be aggressive!", value: "attacking" },
-        { text: "Counter-attacking. I like to wait for a mistake and then fight.", value: "counter-attacking" },
+        {
+          text: "Counter-attacking. I like to wait for a mistake and then fight.",
+          value: "counter-attacking",
+        },
         { text: "Balanced. I believe in good moves.", value: "balanced" },
-        { text: "Positional. I like strategic maneuvering.", value: "positional" },
-        { text: "Trading. I like to win in the endgame.", value: "trading" }
+        {
+          text: "Positional. I like strategic maneuvering.",
+          value: "positional",
+        },
+        { text: "Trading. I like to win in the endgame.", value: "trading" },
       ];
 
       if (this.shouldRemoveTrading()) {
-        options = options.filter(option => option.value !== 'trading');
+        options = options.filter((option) => option.value !== "trading");
       }
 
       return options;
     },
     convertMovesToArray(movesString) {
-      const sanitizedString = movesString.replace(/\d+\./g, '');
+      const sanitizedString = movesString.replace(/\d+\./g, "");
       return sanitizedString.trim().split(/\s+/);
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   text-align: center;
 }
 button {
