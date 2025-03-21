efc807711b8a48471136f4cfbecc05ac26c246d2
simonachess <simonachess@gmail.com>
2025-03-21 14:13:20 +0200

    animation added for testing

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "../src/style/style.css";
 import "../src/style/fonts.css";
-import { MotionPlugin } from "@motionone/vue";
+import { MotionPlugin } from '@vueuse/motion'
 
 const app = createApp(App);
 app.use(MotionPlugin);
